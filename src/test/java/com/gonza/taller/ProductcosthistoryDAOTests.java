30c51600e0c79432cedbb3f28cf75818e89493d3
gonzalodevarona <Billonaire24>
2022-05-19 14:51:37 -0500

    added new test

--- src/test/java/com/gonza/taller/ProductcosthistoryDAOTests.java
+++ src/test/java/com/gonza/taller/ProductcosthistoryDAOTests.java
@@ -1,13 +1,19 @@
 package com.gonza.taller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gonza.taller.dao.LocationDAO;
 import com.gonza.taller.dao.ProductcosthistoryDAO;
@@ -52,5 +58,65 @@ public class ProductcosthistoryDAOTests {
 		
 		
 	}
+	
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Test
+    @Order(1)
+    public void saveProductcosthistoryTest() {
 
-}
+		createProductcosthistory();
+		productcosthistoryDAO.save(productcosthistory);
+		
+        // deberían haber 2 locaciones puesto que el primero fue guardado desde el commandlinerunner
+		
+
+        assertEquals(2, productcosthistoryDAO.findAll().size());
+        
+    }
+	
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Test
+    @Order(2)
+    public void findByIdTest() {
+    	assertEquals(LocalDate.of(2021, 12, 21), productcosthistoryDAO.findById(1).get().getEnddate());	
+    }
+    
+    
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Test
+    @Order(3)
+    public void deleteProductcosthistoryTest() {
+    	
+    	createProductcosthistory();
+		productcosthistoryDAO.save(productcosthistory);
+
+		productcosthistoryDAO.delete(productcosthistoryDAO.findById(2).get());
+        //deberia haber 1 productcosthistory porque el que cree previamente lo acabé de borrar
+        assertEquals(1, productcosthistoryDAO.findAll().size());
+       
+    }
+    
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Test
+    @Order(4)
+    public void editProductcosthistoryTest() {
+
+    	createProductcosthistory();
+		productcosthistoryDAO.save(productcosthistory);
+    	
+    	
+    	assertEquals(LocalDate.of(2021, 12, 21), productcosthistoryDAO.findById(1).get().getEnddate());
+    	
+
+    	Productcosthistory matter = productcosthistoryDAO.findById(1).get();
+    	
+    	matter.setEnddate(LocalDate.of(2021, 12, 30));
+    	
+    	productcosthistoryDAO.update(matter);
+    	
+    	assertEquals(LocalDate.of(2021, 12, 30), productcosthistoryDAO.findById(1).get().getEnddate());
+        
+    }
+	
+
+} //end of class
